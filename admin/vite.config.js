46d8cb6787645e1438d031292f654ce711f0fa47
Ubuntu <ubuntu@ip-10-0-9-245.ap-southeast-1.compute.internal>
2025-08-07 20:52:43 +0000

    Running the Application through docker-compose

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    host: '0.0.0.0',
+    port: 5174, // or 4173, or whatever you map in docker-compose
+  },
   plugins: [react()],
 })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    host: '0.0.0.0',
+    port: 5173, // or 4173, or whatever you map in docker-compose
+  },
+
   plugins: [react()],
 })
